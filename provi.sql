17b8e1d56152e0f1e08d0519a7a010e3aa015b7e
oversanzero <oversanzero@gmail.com>
2024-08-30 04:26:50 +0000

    database - more users and products not really

--- provi.sql
+++ provi.sql
@@ -16,7 +16,28 @@ CREATE TABLE products (
 -- Insertar datos de ejemplo
 INSERT INTO users (username, email)
 VALUES ('user1', 'user1@example.com'),
-  ('user2', 'user2@example.com');
+  ('user2', 'user2@example.com'),
+  ('user3', 'user3@example.com'),
+  ('user4', 'user4@example.com'),
+  ('user5', 'user5@example.com'),
+  ('user6', 'user6@example.com'),
+  ('user7', 'user7@example.com'),
+  ('user8', 'user8@example.com'),
+  ('user9', 'user9@example.com');
 INSERT INTO products (name, description, price)
 VALUES ('Product 1', 'This is product 1', 10.99),
-  ('Product 2', 'This is product 2', 19.99);
+  ('Product 2', 'This is product 2', 19.99),
+  ('Product 3', 'This is product 3', 29.99),
+  ('Product 4', 'This is product 4', 39.99),
+  ('Product 5', 'This is product 5', 49.99),
+  ('Product 6', 'This is product 6', 59.99),
+  ('Product 7', 'This is product 7', 69.99),
+  ('Product 8', 'This is product 8', 79.99),
+  ('Product 9', 'This is product 9', 89.99),
+  ('Product 10', 'This is product 10', 99.99),
+  ('Product 11', 'This is product 11', 109.99),
+  ('Product 12', 'This is product 12', 119.99),
+  ('Product 13', 'This is product 13', 129.99),
+  ('Product 14', 'This is product 14', 139.99),
+  ('Product 15', 'This is product 15', 149.99),
+  ('Product 16', 'This is product 16', 159.99);
